9c14752d7275acc9972f392c046053801abd4b6e
David Churchill <dave.churchill@gmail.com>
2019-02-05 21:05:25 -0330

    sensor refactor 2

--- src/Sensors.h
+++ src/Sensors.h
@@ -2,8 +2,9 @@
 
 #include <memory>
 
-class World;
-
+#include "World.hpp"
+#include "Entity.hpp"
+#include "Components.hpp"
 
 class Sensor
 {
@@ -15,12 +16,27 @@ protected:
 
 public:
 
-    Sensor();
-    Sensor(size_t ownerID, double angle, double distance);
+    Sensor() {}
+    Sensor(size_t ownerID, double angle, double distance)
+        : m_ownerID(ownerID), m_angle(angle*3.1415926 / 180.0), m_distance(distance) { }
 
-    Vec2 getPosition();
-    virtual double angle() const;
-    virtual double distance() const;
+    virtual Vec2 getPosition()
+    {
+        const Vec2 & pos = Entity(m_ownerID).getComponent<CTransform>().p;
+        double sumAngle = m_angle + Entity(m_ownerID).getComponent<CSteer>().angle;
+        return pos + Vec2(m_distance * cos(sumAngle), m_distance * sin(sumAngle));
+    }
+
+    virtual double angle() const
+    {
+        return m_angle;
+    }
+
+    virtual double distance() const
+    {
+        return m_distance;
+    }
+
     virtual double getReading(std::shared_ptr<World> world) = 0;
 };
 
@@ -30,8 +46,17 @@ class GridSensor : public Sensor
     
 public:
 
-    GridSensor(size_t ownerID, double angle, double distance);
-    virtual double getReading(std::shared_ptr<World> world);
+    GridSensor(size_t ownerID, double angle, double distance)
+        : Sensor(ownerID, angle, distance) {}
+
+    virtual double getReading(std::shared_ptr<World> world)
+    {
+        if (world->getGrid().width() == 0) { return 0; }
+        Vec2 sPos = getPosition();
+        size_t gX = (size_t)round(world->getGrid().width()  * sPos.x / world->width());
+        size_t gY = (size_t)round(world->getGrid().height() * sPos.y / world->height());
+        return world->getGrid().get(gX, gY);
+    }
 };
 
 
@@ -41,9 +66,35 @@ class PuckSensor : public Sensor
 
 public:
 
-    PuckSensor(size_t ownerID, double angle, double distance, double radius);
-    virtual double getReading(std::shared_ptr<World> world);
-    double radius() const;
+    PuckSensor(size_t ownerID, double angle, double distance, double radius)
+        : Sensor(ownerID, angle, distance)
+    {
+        m_radius = radius;
+    }
+
+    double getReading(std::shared_ptr<World> world)
+    {
+        double sum = 0;
+        Vec2 pos = getPosition();
+        for (auto puck : world->getEntities("puck"))
+        {
+            auto & t = puck.getComponent<CTransform>();
+            auto & b = puck.getComponent<CCircleBody>();
+
+            // collision with a puck
+            if (t.p.distSq(pos) < (m_radius + b.r)*(m_radius + b.r))
+            {
+                sum += 1.0;
+            }
+        }
+        return sum;
+    }
+
+    double radius() const
+    {
+        return m_radius;
+    }
+
 };
 
 class ObstacleSensor : public Sensor
@@ -52,7 +103,35 @@ class ObstacleSensor : public Sensor
 
 public:
 
-    ObstacleSensor(size_t ownerID, double angle, double distance, double radius);
-    virtual double getReading(std::shared_ptr<World> world);
-    double radius() const;
+    ObstacleSensor(size_t ownerID, double angle, double distance, double radius)
+        : Sensor(ownerID, angle, distance)
+    {
+        m_radius = radius;
+    }
+
+    double getReading(std::shared_ptr<World> world)
+    {
+        double sum = 0;
+        Vec2 pos = getPosition();
+        for (auto e : world->getEntities())
+        {
+            if (!e.hasComponent<CCircleBody>()) { continue; }
+            if (m_ownerID == e.id()) { continue; }
+
+            auto & t = e.getComponent<CTransform>();
+            auto & b = e.getComponent<CCircleBody>();
+
+            // collision with a puck
+            if (t.p.distSq(pos) < (m_radius + b.r)*(m_radius + b.r))
+            {
+                sum += 1.0;
+            }
+        }
+        return sum;
+    }
+
+    double radius() const
+    {
+        return m_radius;
+    }
 };
